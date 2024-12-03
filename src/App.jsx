3b312a348ac8970d00a9dfa3b308ace0541649d4
NTZ Nusrat <nusratf555@gmail.com>
2024-12-03 21:20:03 +0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import { Routes, Route } from 'react-router'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </>
+  )
+}
+
+export default App
